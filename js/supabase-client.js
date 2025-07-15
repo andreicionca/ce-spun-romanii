30c5aacc29771921ce46c2389590970f73123fba
Andrei Cionca <andreicionca1@gmail.com>
2025-07-16 01:26:59 +0300

    Add control and display pages with Supabase integration

---
+++ js/supabase-client.js
@@ -0,0 +1,18 @@
+// js/supabase-client.js
+(function () {
+  const supabaseUrl = "https://aygjiwialyfonpewdxqw.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Z2ppd2lhbHlmb25wZXdkeHF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTUzNTUsImV4cCI6MjA2ODE5MTM1NX0.bzFrIpBvqWW4iZEJqctWCXgDzY2iFG8Vv64pfm1s8kY";
+  // supabase este acum global
+  window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
+  console.log("✅ Supabase initialized");
+})();
+// js/supabase-client.js
+(function () {
+  const supabaseUrl = "https://aygjiwialyfonpewdxqw.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Z2ppd2lhbHlmb25wZXdkeHF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTUzNTUsImV4cCI6MjA2ODE5MTM1NX0.bzFrIpBvqWW4iZEJqctWCXgDzY2iFG8Vv64pfm1s8kY";
+  // supabase este acum global
+  window.supabase = supabase.createClient(supabaseUrl, supabaseKey);
+  console.log("✅ Supabase initialized");
+})();
